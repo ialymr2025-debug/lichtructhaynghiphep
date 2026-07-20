ccd943509f10cb4405e725673b196de0698a0d1e
ialymr2025-debug <ialymr2025@gmail.com>
2026-07-20 14:11:49 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -62,7 +62,7 @@ export default function App() {
     soVanBan: '',
     ngayKy: '',
     nguoiKy: 'Nguyễn Văn Nghị',
-    zaloWebhookUrl: 'https://committed-intellectual-lunch-clone.trycloudflare.com/webhook/notify'
+    zaloWebhookUrl: 'https://vhialy.dpdns.org/webhook/notify'
   });
 
   const [isGoogleAuth, setIsGoogleAuth] = useState(false);
@@ -179,7 +179,7 @@ export default function App() {
             soVanBan: data.config.soVanBan || '',
             ngayKy: data.config.ngayKy || '',
             nguoiKy: data.config.nguoiKy || 'Nguyễn Văn Nghị',
-            zaloWebhookUrl: data.config.zaloWebhookUrl || 'https://committed-intellectual-lunch-clone.trycloudflare.com/webhook/notify'
+            zaloWebhookUrl: data.config.zaloWebhookUrl || 'https://vhialy.dpdns.org/webhook/notify'
           });
         }
 
